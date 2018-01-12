ce901c4945bf29cc896517f421f879bd4cc65188
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-12 01:05:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !BRANDS.include(brand)
-          BRANDS << brand
+    if BRANDS.include(brand)
+        next
     end
+      BRANDS << brand
   end
 
   def cobble
